ef67208c4a7b7500e5afa8a8ff1247f5264dcd3f
Joe Terlecki <70246976+joeterlecki@users.noreply.github.com>
2021-03-10 06:38:57 -0500

    New repos for clean history (#1)
    Co-authored-by: Joe Terlecki <joe.p.terlecki@gmail.com>

---
+++ main.tf
@@ -0,0 +1,34 @@
+resource "aws_s3_bucket" "standard_bucket" {
+  bucket = var.bucket_name
+  acl    = var.canned_acl
+  tags   = local.tags
+  versioning {
+    enabled = var.versioning_status
+  }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+
+  dynamic "logging" {
+    for_each = length(keys(var.logging)) == 0 ? [] : [var.logging]
+
+    content {
+      target_bucket = logging.value.target_bucket
+      target_prefix = lookup(logging.value, "target_prefix")
+    }
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "public_access" {
+  bucket = aws_s3_bucket.standard_bucket.id
+
+  block_public_acls       = var.block_public_acls
+  block_public_policy     = var.block_public_policy
+  ignore_public_acls      = var.ignore_public_acls
+  restrict_public_buckets = var.restrict_public_buckets
+}
